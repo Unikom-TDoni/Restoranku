92faa9de5d4214b884ee8ff353ae0ccb19b41ef0
Theodorus Doni <theodorusdoni4@gmail.com>
2023-02-01 21:01:11 +0700

    Add logo

--- app/src/main/java/edu/unikom/restoranku/core/MainActivity.kt
+++ app/src/main/java/edu/unikom/restoranku/core/MainActivity.kt
@@ -2,11 +2,32 @@ package edu.unikom.restoranku.core
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import edu.unikom.restoranku.R
 
 class MainActivity : AppCompatActivity() {
+
+    private var isScreenReady = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initSplashScreen()
+        startSplashScreenTimer()
         setContentView(R.layout.activity_main)
     }
+
+    private fun startSplashScreenTimer() {
+        val timer = object : CountDownTimer(2000, 1000) {
+            override fun onTick(p0: Long) {}
+            override fun onFinish() {
+                isScreenReady = true
+            }
+        }
+        timer.start()
+    }
+
+    private fun initSplashScreen() =
+        installSplashScreen().apply { setKeepOnScreenCondition { !isScreenReady } }
+
 }
